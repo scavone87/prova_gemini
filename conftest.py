78e913a5753bec77fa96b905960b71a8d897dab4
rocco <r.scavone@tabsrl.com>
2025-04-08 23:05:24 +0200

    Aggiunto fixture funnel_id per risolvere i problemi nei test

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ import os
 def funnel_id():
     """
     Fixture che fornisce un ID di funnel valido per i test.
-
+    
     Può essere sovrascritto passando la variabile d'ambiente TEST_FUNNEL_ID.
     Altrimenti, utilizza un ID di default (1).
     """
@@ -16,6 +16,6 @@ def funnel_id():
     env_funnel_id = os.environ.get('TEST_FUNNEL_ID')
     if env_funnel_id:
         return int(env_funnel_id)
-
+    
     # Altrimenti, utilizza un ID di default
     return 1
